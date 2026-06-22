86004276848ea16fb31f5c7cc5b3c82937f26687
Navneet <conavneet@gmail.com>
2026-06-22 10:42:37 +0000

    Create branch phonebook

--- part1/courseinfo/src/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = (props) => {
-    return (
-        <p>Number of exercises {props.sum}</p>
-    )
-}
-
-export default Total
-
--- part1/unicafe/src/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = (props) => {
-    return (
-        <>
-            <button onClick={() => props.setValue(props.value+1)}>{props.text}</button>
-        </>
-    )
-}
-
-export default Button
